ff77e65010894505d1e46ec70f32917f5ae870cf
Diegocorp <diegosandovalr.corp@gmail.com>
2022-03-31 12:12:21 -0700

    template complete

---
+++ index.js
@@ -0,0 +1,23 @@
+//ANCHOR: Multiplying two numbers without using the multiplication symbol
+
+//ANCHOR: Get the biggest number of an array
+
+//ANCHOR: Clear an array of the values "0, null, undefined and false"
+
+//ANCHOR: Flatten the array one level
+
+//ANCHOR: Identify the most repeated word in the string
+
+//ANCHOR: Checking whether a string is a palindrome or not
+
+//ANCHOR: Remove duplicate elements in an array
+
+//ANCHOR: Print numbers from 1 to 100 (multiplies of 3 print "Fizz" of 5 print "Buzz" and multiplies of 3 and 5 print "FizzBuzz").
+
+//ANCHOR: Create an object with a method that prints by console "Hi world".
+
+//ANCHOR: Creates an object with properties and prevents it from being modified
+
+//ANCHOR: Creates an object and prevents new properties from being added to it
+
+//ANCHOR: Declares the paradox of "for" with "setTimeOut" with let and var
